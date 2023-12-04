c9c530baae20f0d8013a51443418944950ffc7db
Fedja Pandzic <fedjapandzic1@gmail.com>
2023-12-04 13:51:41 +0100

    deleted files

---
+++ config_default.php
@@ -0,0 +1,16 @@
+<?php
+class Config{
+    public static $DB_HOST='dpg-clmcm49fb9qs739bha90-a.frankfurt-postgres.render.com';
+    public static $DB_USERNAME='root';
+    public static $DB_PASSWORD='btIhWIuejAwjzdpGeSPaM4DPgqPspZ5T';
+    public static $DB_NAME='sssd';
+    public static $DB_PORT='5432';
+
+    public static $SMTP_HOST='';
+    public static $SMTP_PORT='';
+    public static $SMTP_USERNAME='';
+    public static $SMTP_PASSWORD="";
+    public static $NEXMO_API_KEY='';
+    public static $NEXMO_API_SECRET='';
+
+}
