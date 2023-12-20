2a2d87ee191ee352b9e3ff9e7779895035e300fd
Fedja Pandzic <fedjapandzic1@gmail.com>
2023-12-20 18:59:44 +0100

    fix

--- backend/UserAuthenticationRoutes.php
+++ backend/UserAuthenticationRoutes.php
@@ -1,4 +1,19 @@
 <?php
+
+require './vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$host = getenv('DB_HOST');
+$user = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$port = getenv('DB_PORT');
+$dbname = getenv('DB_NAME');
+$failed_attempts = isset($_SESSION['failed_attempts']) ? $_SESSION['failed_attempts'] : 0;
+
+$db=pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
 Flight::route('POST /registracija', function(){
     
     global $db;
--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
 session_start();
 
-require 'vendor/autoload.php';
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-$host = getenv('DB_HOST');
-$user = getenv('DB_USERNAME');
-$password = getenv('DB_PASSWORD');
-$port = getenv('DB_PORT');
-$dbname = getenv('DB_NAME');
-$failed_attempts = isset($_SESSION['failed_attempts']) ? $_SESSION['failed_attempts'] : 0;
-
-$db=pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 include 'backend/RenderRoutes.php';
 include 'backend/UserAuthenticationRoutes.php';
