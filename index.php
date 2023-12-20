b1a3f66e4bc9175ea1eb7baeb1e324738e15ae17
Fedja Pandzic <fedjapandzic1@gmail.com>
2023-12-20 18:09:39 +0100

    changed rendering routes to separate folder

--- index.php
+++ index.php
@@ -18,9 +18,6 @@ $failed_attempts = isset($_SESSION['failed_attempts']) ? $_SESSION['failed_attem
 $db=pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 
-Flight::route('/', function(){
-    include 'html/register.html';
-});
 
 Flight::route('POST /registracija', function(){
     
@@ -202,33 +199,12 @@ Flight::route('/verify/@EVT', function($EVT){
     }
 });
 
-Flight::route('/UserVerified', function(){
-    echo '<script>alert("You have been verified!")</script>';
-    global $failed_attempts;
-    include 'html/login.html';
-});
 
-Flight::route('/homeRoute', function(){
-    if(isset($_SESSION['full_name']) && $_SESSION['is_verified'] == 1){
-        include 'html/home.html';
-    }
-    else {
-        Flight::redirect('/login');
-    }
-});
 
-Flight::route('/login', function(){
-    global $failed_attempts;
-    include 'html/login.html';
-});
 
-Flight::route('/checkyouremail' , function(){
-    include 'html/checkverification.html';
-});
 
-Flight::route('/changePassword', function(){
-    include 'html/changepass.html';
-});
+
+
 
 Flight::route('POST /passwordChange', function(){
     global $db;
@@ -262,19 +238,7 @@ Flight::route('POST /passwordChange', function(){
     }
 });
 
-Flight::route('/twofactorauthenticator', function(){
-    if (isset($_SESSION['phone_number'])) {
-        include 'html/twofactorauth.html';
-    } else {
-        // Redirect to login or handle the case where phone_number is not set.
-        Flight::redirect('/login');
-    }
 
-});
-
-Flight::route('/forgotpassword',function(){
-    include 'html/forgotpassword.html';
-});
 
 Flight::route('POST /sendNewPass',function(){
     global $db;
@@ -326,12 +290,7 @@ Flight::route('POST /sendNewPass',function(){
     }
 });
 
-Flight::route('/logout', function(){
-    session_unset();
-    if(!isset($_SESSION['phone_number']) && !isset($_SESSION['full_name'])){
-        Flight::redirect('/login');
-    }
-});
+
 
 Flight::route('POST /sendSMSCode', function(){
     $code = str_pad(rand(0, pow(10, 4)-1), 4, '0', STR_PAD_LEFT);
